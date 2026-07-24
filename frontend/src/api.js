46d33bc3f1dc032973545c9c8e578b881faf07bd
ZanirP <46696445+ZanirP@users.noreply.github.com>
2026-07-24 16:13:53 -0500

    fix: fixing endpoints of frontend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const API_BASE_URL = 
+  process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:8000' 
+    : window.location.origin;
+  
 const api = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: API_BASE_URL,
 });
 
 
